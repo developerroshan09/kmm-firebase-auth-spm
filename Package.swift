45b4b076fc85cd7e13ef3f4f55f91e0129ca0c2a
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2026-01-11 16:11:03 +0545

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:6.2
+import  PackageDescription
+
+let package = Package(
+    name: "KmmFirebaseAuth",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "KmmFirebaseAuth",
+            targets: ["KmmFirebaseAuth"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "KmmFirebaseAuth",
+            url: "https://github.com/developerroshan09/xcframework_dist_spm/releases/download/1.1.3/ComposeApp.xcframework.zip",
+            checksum: "7d7440e4242b56e0189a02365c268bc0c81f206e496a305e60ded9374f1e6595"
+        )
+    ]
+)
