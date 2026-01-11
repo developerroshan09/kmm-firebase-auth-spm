17b3e7418c18f5c2b16540ecb5d2d49c4f06d091
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2026-01-11 18:31:07 +0545

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KmmFirebaseAuth",
-            url: "https://github.com/developerroshan09/xcframework_dist_spm/releases/download/1.0.3/KmmFirebaseAuth.xcframework.zip",
+            url: "https://github.com/developerroshan09/kmm-firebase-auth-spm/releases/download/1.0.4/KmmFirebaseAuth.xcframework.zip",
             checksum: "29cc1fb117548ebda13b4feafebca69c6a21cfcc7fd9e9cbc8509e38f9a309d4"
         )
     ]
